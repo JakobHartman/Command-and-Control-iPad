a9d11f33504fdc650fa7ece153f16fe1843d8eab
Steven <stsmith07@gmail.com>
2014-01-20 15:15:15 -0700

    Initial Commit

---
+++ Command and Control iPadTests/Command_and_Control_iPadTests.m
@@ -0,0 +1,34 @@
+//
+//  Command_and_Control_iPadTests.m
+//  Command and Control iPadTests
+//
+//  Created by Steven Smith on 1/20/14.
+//  Copyright (c) 2014 Whirling Penguins. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Command_and_Control_iPadTests : XCTestCase
+
+@end
+
+@implementation Command_and_Control_iPadTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
